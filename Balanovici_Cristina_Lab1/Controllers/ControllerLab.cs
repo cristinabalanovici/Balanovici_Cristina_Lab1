7acf8d459e0bf64c285511574bfa21d61137fa2e
cristinabalanovici <cristina.balanovici@yahoo.com>
2022-10-11 19:23:00 +0300

    Prima Pagina modificata

---
+++ Balanovici_Cristina_Lab1/Controllers/ControllerLab.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Balanovici_Cristina_Lab1.Controllers
+{
+    public class ControllerLab : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Bun venit"); 
+        }
+
+        public IActionResult Salut()
+        {
+            return Content("Salut");
+        }
+
+        public IActionResult SalutMesaj(string nume, int varsta)
+        {
+            return Content("Salut " +nume + " Ai " +varsta+" ani");
+        }
+    }
+}
